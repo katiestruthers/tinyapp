32981a918040a0e47500e95ed5b8fc3c9eb29541
Katherine Struthers <ktstruthers@gmail.com>
2023-05-28 16:23:49 -0500

    create generateRandomString function

--- express_server.js
+++ express_server.js
@@ -3,11 +3,23 @@ const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
+
+const generateRandomString = function() {
+  let string = '';
+
+  for (let i = 0; i < 6; i++) {
+    string += String.fromCharCode(Math.floor(Math.random() * 95) + 33);
+  }
+
+  return string;
+};
+console.log(generateRandomString());
 
 app.get('/', (request, response) => {
   response.send('Hello!');
@@ -16,6 +28,10 @@ app.get('/', (request, response) => {
 app.get('/urls', (request, response) => {
   const templateVars = { urls: urlDatabase };
   response.render('urls_index', templateVars);
+});
+
+app.get('/urls/new', (request, response) => {
+  response.render('urls_new');
 });
 
 app.get('/urls/:id', (request, response) => {
@@ -29,6 +45,11 @@ app.get('/urls.json', (request, response) => {
 
 app.get('/hello', (request, response) => {
   response.send('<html><body>Hello <b>World</b></body></html>\n');
+});
+
+app.post('/urls', (request, response) => {
+  console.log(request.body);
+  response.send('Ok');
 });
 
 app.listen(PORT, () => {
